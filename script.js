492708d762857da84bdeb72bc29b2a2b45c1089e
unknown <90293855+fishdestroyer@users.noreply.github.com>
2021-10-17 21:50:22 -0500

    Updated code with prompts

--- script.js
+++ script.js
@@ -4,29 +4,24 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var numbers = ["0,1,2,3,4,5,6,7,8,9"];
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q",
+"R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var specialCharacter = ["!", "@", "#", "$", "<", "(", ")", "&", "*", "?", "+", "=", "^"];
 
 // Write password to the #password input
-function generatePassword() {
-  var length=(enter <8 || enter> 128);
-  
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
 document.getElementById("generate").addEventListener("click", myFunction);
 
 function myFunction() {
-   parseInt(prompt("How many characters would you like your password? Choose between 8 and 128"));
+  password = parseInt(prompt("How many characters would you like your password? Choose between 8 and 128"));
+if (!password) {
+  prompt ("A value is required");
+
+} else if (password < 8 || password > 128) {
+   password = parseInt(prompt("Must contain between 8 and 28"));
 }
-
-
-
-
-
-    // Asks for user input
-   //function generatePassword() {
-   // enter = parseInt(prompt("How many characters would you like your password? Choose between 8 and 128"));
-
-// Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+}
+for (var i = 0; i < password; i++) {
+   var pickChoices = choices[Math.floor(Math.random() * choices.length)];
+   password.push(pickChoices);
+}
